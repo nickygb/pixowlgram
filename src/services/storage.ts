46b37dc5413a96a192dbbb7dc15f269853f36704
nickygb <nicogrunfeld@gmail.com>
2020-12-20 20:18:39 -0300

    storage service + unit testing

---
+++ src/services/storage.ts
@@ -0,0 +1,19 @@
+import { S3 } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+
+export const makeUploadService = (client: S3, bucket: string, folder: string) => {
+  return async (photo: string): Promise<string> => {
+    const key = `${folder}/${uuid()}`;
+    const buffer = Buffer.from(photo.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+    await client
+      .putObject({
+        Bucket: bucket,
+        Key: key,
+        Body: buffer,
+        ContentEncoding: 'base64',
+      })
+      .promise();
+    const photoUrl = `https://${process.env.AWS_REGION || 'us-east-1'}.amazonaws.com/${bucket}/${key}`;
+    return photoUrl;
+  };
+};

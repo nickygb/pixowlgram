feb2db5b42a73218d8017b12f75c3fa92c74df30
nickygb <nicogrunfeld@gmail.com>
2020-12-21 17:28:17 -0300

    pagination using cursor + tests

--- src/models/post.ts
+++ src/models/post.ts
@@ -12,8 +12,38 @@ interface IPost {
   description: string;
   photo: string;
   likes: number;
+  user_id: number;
+  created_at: string | Date;
+}
+
+interface Cursor {
+  post_id: number;
   created_at: string;
 }
+
+interface PaginatedPosts {
+  posts: IPost[];
+  nextCursor: string;
+}
+
+const postsBetweenDatesQuery = (connection: Knex, startDate: Date, endDate: Date): any => {
+  const query = connection
+    .from('post')
+    .select('*')
+    .where('created_at', '>=', startDate)
+    .andWhere('created_at', '<', endDate);
+  return query;
+};
+
+export const decodeCursor = (encodedCursor: string): Cursor => {
+  const decodedCursor = JSON.parse(Buffer.from(encodedCursor, 'base64').toString('ascii'));
+  return decodedCursor;
+};
+const encodeCursor = (postId: number, createdAt: string): string => {
+  const cursor: Cursor = { post_id: postId, created_at: createdAt };
+  const encoded = Buffer.from(JSON.stringify(cursor)).toString('base64');
+  return encoded;
+};
 
 export class Post {
   private _connection: Knex;
@@ -30,13 +60,52 @@ export class Post {
     return posts[0];
   }
 
-  async getAll(): Promise<IPost[]> {
-    const res: IPost[] = await this._connection
-      .select('p.id', 'p.description', 'p.likes', 'p.created_at', 'ph.url as photo')
+  async getAll(encodedCursor?: string, limit = 10): Promise<PaginatedPosts> {
+    const query = this._connection
+      .select('p.id', 'p.description', 'p.likes', 'p.created_at', 'ph.url as photo', 'p.user_id')
       .from('post as p')
       .join('photo as ph', 'ph.post_id', 'p.id')
+      .limit(limit + 1) // +1 to get the next cursor
+      .orderBy('p.created_at', 'desc')
+      .orderBy('p.id', 'desc');
+
+    if (encodedCursor && encodedCursor !== '') {
+      try {
+        const cursor = decodeCursor(encodedCursor);
+        query.where('p.id', '<=', cursor.post_id).where('p.created_at', '<=', cursor.created_at);
+      } catch (err) {
+        // Invalid cursor ... return most recent posts
+      }
+    }
+    const posts: IPost[] = await query;
+
+    // We create the next cursor with the last post
+    let nextCursorEncoded = undefined;
+    if (posts.length > limit) {
+      const nextPost = posts.pop();
+      nextCursorEncoded = encodeCursor(nextPost.id, nextPost.created_at as string);
+    }
+    return {
+      posts,
+      nextCursor: nextCursorEncoded,
+    };
+  }
+
+  async getAllOld(startDate: Date, endDate: Date, limit: number): Promise<IPost[]> {
+    const query = this._connection
+      .select('p.id', 'p.description', 'p.likes', 'p.created_at', 'ph.url as photo')
+      .from({
+        // This query allows as to take advantage of mysql query caching
+        // because we are first selecting all the posts in the range of dates
+        // and after this we perform the limit in the outer query.
+        p: postsBetweenDatesQuery(this._connection, startDate, endDate),
+      })
+      .join('photo as ph', 'ph.post_id', 'p.id')
+      .limit(limit)
       .orderBy('p.created_at', 'desc');
-    return res;
+
+    const posts: IPost[] = await query;
+    return posts;
   }
 
   async add(newPost: NewPost): Promise<IPost> {
--- test/unit/model.post.test.ts
+++ test/unit/model.post.test.ts
@@ -3,9 +3,10 @@ import chaiDateTime from 'chai-datetime';
 const expect = chai.expect;
 chai.use(chaiDateTime);
 import Knex from 'knex';
-import { Post } from '../../src/models/post';
+import { Post, decodeCursor } from '../../src/models/post';
 import knexfile from '../../src/knexfile';
 import faker from 'faker';
+import { range } from '../lib/utils';
 
 const knexConfig = { ...knexfile };
 knexConfig.connection.filename = './mydb.sqlite';
@@ -23,6 +24,15 @@ const addHours = (date: Date) => (hours) => {
   const d = new Date(date);
   return new Date(d.setTime(d.getTime() + hours * 60 * 60 * 1000));
 };
+
+const checkIfDuplicatePost = (posts) => new Set(posts.map((p) => p.id)).size !== posts.length;
+
+const fakeNewPost = (createdAt) => ({
+  description: faker.lorem.text(),
+  photoUrl: faker.image.imageUrl(),
+  createdAt,
+  userId: faker.random.number(),
+});
 
 const model = new Post(knex);
 describe('Post model', function () {
@@ -45,7 +55,7 @@ describe('Post model', function () {
       };
       const post = await model.add(newPost);
       const nowDate = new Date(new Date().toUTCString());
-      const createdAtDate = parseUtcStringDate(post.created_at);
+      const createdAtDate = parseUtcStringDate(post.created_at as string);
 
       expect(post.description).to.be.equal(newPost.description);
       expect(post.id).to.be.a('number');
@@ -83,35 +93,118 @@ describe('Post model', function () {
   });
 
   describe('--> getAll', function () {
-    it('--> Should return a list of posts ordered by created_at descendent', async () => {
-      const nowUtcAddHours = addHours(new Date());
-      const newPost1 = {
-        description: faker.lorem.text(),
-        photoUrl: faker.image.imageUrl(),
-        createdAt: nowUtcAddHours(0),
-        userId: faker.random.number(),
-      };
-      const newPost2 = {
-        description: faker.lorem.text(),
-        photoUrl: faker.image.imageUrl(),
-        createdAt: nowUtcAddHours(5),
-        userId: faker.random.number(),
-      };
-      const newPost3 = {
-        description: faker.lorem.text(),
-        photoUrl: faker.image.imageUrl(),
-        createdAt: nowUtcAddHours(2),
-        userId: faker.random.number(),
-      };
-      const addedPosts = await Promise.all([model.add(newPost1), model.add(newPost2), model.add(newPost3)]);
-      const posts = await model.getAll();
-      expect(posts[0].id).to.be.equal(addedPosts[1].id);
-      expect(posts[1].id).to.be.equal(addedPosts[2].id);
+    it('--> Should return a list of posts ordered chronologically (from newest to oldest)', async () => {
+      const now = new Date();
+      const nowUtcAddHours = addHours(now);
+      const inTwoHours = nowUtcAddHours(2);
+      const inFiveHours = nowUtcAddHours(5);
+      const newPostNow = fakeNewPost(now);
+      const newPostInFiveHours = fakeNewPost(inFiveHours);
+      const newPostInTwoHours = fakeNewPost(inTwoHours);
+
+      const addedPosts = await Promise.all([
+        model.add(newPostNow),
+        model.add(newPostInTwoHours),
+        model.add(newPostInFiveHours),
+      ]);
+      const { posts, nextCursor } = await model.getAll();
+      expect(posts[0].id).to.be.equal(addedPosts[2].id);
+      expect(posts[1].id).to.be.equal(addedPosts[1].id);
       expect(posts[2].id).to.be.equal(addedPosts[0].id);
+      expect(nextCursor).to.be.an('undefined');
     });
 
-    it('--> Should return paginated list of posts');
+    it('--> Pagination should works properly when there is posts created at same time', async () => {
+      const now = new Date();
+      const inTwoHours = addHours(now)(2);
+      const somePostsNow = await Promise.all(
+        range(4) // Add 4 posts now
+          .map(() => fakeNewPost(now))
+          .map(model.add, model)
+      );
+      const somePostsInTwoHours = await Promise.all(
+        range(10) // Add 10 posts in two hours
+          .map(() => fakeNewPost(inTwoHours))
+          .map(model.add, model)
+      );
 
-    it('--> Should return the next page using offset');
+      const { posts, nextCursor } = await model.getAll(undefined, 10);
+      // posts here should be all in two hours and not repeated
+      expect(checkIfDuplicatePost(posts)).to.be.equal(false);
+      posts.forEach((post) => {
+        expect(new Date(post.created_at)).to.be.closeToTime(inTwoHours, 1);
+      });
+
+      // If I get the next posts using the nextCursor should get all
+      const { posts: posts2, nextCursor: nextCursor2 } = await model.getAll(nextCursor, 10);
+      expect(posts2.length).to.be.equal(4);
+      expect(nextCursor2).to.be.equal(undefined);
+      expect(checkIfDuplicatePost(posts2)).to.be.equal(false);
+    });
+
+    it('--> Should return maximum of 10 posts if limit is not specified', async () => {
+      const now = new Date();
+      await Promise.all(
+        range(15) // Add 15 posts now
+          .map(() => fakeNewPost(now))
+          .map(model.add, model)
+      );
+      const { posts } = await model.getAll();
+      expect(posts.length).to.be.equal(10);
+    });
+
+    it('--> Should return at most a number of posts specified by limit', async () => {
+      const now = new Date();
+      const nowUtcAddHours = addHours(now);
+      const inTenHours = nowUtcAddHours(10);
+      const inFiveHours = nowUtcAddHours(5);
+      const newPostNow = fakeNewPost(now);
+      const newPostInFiveHours = fakeNewPost(inFiveHours);
+      const newPostInTenHours = fakeNewPost(inTenHours);
+
+      await Promise.all([model.add(newPostNow), model.add(newPostInFiveHours), model.add(newPostInTenHours)]);
+      const limit = 2;
+      const { posts } = await model.getAll(undefined, limit);
+      expect(posts.length).to.be.equal(limit);
+    });
+
+    it('--> Should return the nextCursor base64 encoded', async () => {
+      const now = new Date();
+      const nowUtcAddHours = addHours(now);
+      const inTwoHours = nowUtcAddHours(2);
+      const inFiveHours = nowUtcAddHours(5);
+      const newPostNow = fakeNewPost(now);
+      const newPostInFiveHours = fakeNewPost(inFiveHours);
+      const newPostInTwoHours = fakeNewPost(inTwoHours);
+
+      const addedPosts = await Promise.all([
+        model.add(newPostNow),
+        model.add(newPostInFiveHours),
+        model.add(newPostInTwoHours),
+      ]);
+
+      const limit = 2;
+      const { nextCursor } = await model.getAll(undefined, limit);
+      expect(nextCursor).to.be.an('string');
+      const nextCursorDecoded = decodeCursor(nextCursor);
+      expect(nextCursorDecoded.post_id).to.be.equal(addedPosts[0].id);
+      expect(new Date(nextCursorDecoded.created_at)).to.be.closeToTime(addedPosts[0].created_at as Date, 0.1);
+    });
+
+    it('--> If nextCursor is invalid, return most recent posts', async () => {
+      const now = new Date();
+      const nowUtcAddHours = addHours(now);
+      const inTwoHours = nowUtcAddHours(2);
+      const inFiveHours = nowUtcAddHours(5);
+      const newPostNow = fakeNewPost(now);
+      const newPostInFiveHours = fakeNewPost(inTwoHours);
+      const newPostInTenHours = fakeNewPost(inFiveHours);
+
+      await Promise.all([model.add(newPostNow), model.add(newPostInFiveHours), model.add(newPostInTenHours)]);
+
+      const nextCursor = 'InvalidNextCursor';
+      const { posts } = await model.getAll(nextCursor);
+      expect(posts.length).to.be.equal(3);
+    });
   });
 });
